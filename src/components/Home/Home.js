b5679574e07c4be948c37785a5365b6eb74a45f7
jrtanmoy <roy28301@gmail.com>
2022-04-04 01:10:48 +0600

    creat json data & apply

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css'
+import Review from '../Review/Review';
 
 const Home = () => {
+    const [reviews,setreviews] = useState([])
+    useEffect( () => {
+        fetch('review-data.json')
+        .then(res=> res.json())
+        .then(data => setreviews(data))
+    },[])
+
     return (
-        <div>
+        <div className='container'>
             <h1 className='h1-title-style'>Guiter review !!!</h1>
-            <div className="container">
-                <div className="container-info">
+            <div className="first-section-container">
+                <div className="first-section-container-info">
                     <h2>Your next Guiter</h2>
                     <h2 className='h2-style'>Your Best Guiter</h2>
                     <p>The Epiphone J200 EC Studio pays homage to its Gibson counterpart, with stunning vintage correct stylings. The moustache bridge, decorative tortoiseshell-style pickguard and pearloid crown inlays add a level of class to this instrument and we must say, it sounds as good as it looks, as well. The sound is balanced rather than boomy, giving strummed chords the fullness that made the J-200's reputation. Expect articulate highs and a warm foundation in the bottom end. </p>
@@ -14,6 +22,18 @@ const Home = () => {
                 <div className="">
                     <img src="https://m.media-amazon.com/images/I/71jQsSC3ovL._AC_SX679_.jpg" alt="" />
                 </div>
+            </div>
+            <div className='review-container'>
+                <h1 className='h1-title-style'>Customer Reviews(3)</h1>
+                <div className='review-card'>
+                    {
+                        reviews.map(review =><Review
+                            key={review.id}
+                            review={review}
+                        ></Review>)
+                    }
+                </div>
+                
             </div>
         </div>
     );

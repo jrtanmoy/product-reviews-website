bbd92e85948eff41abbee9ae0c5daf58d9a97884
jrtanmoy <roy28301@gmail.com>
2022-04-04 12:19:09 +0600

    install fontAwesomeIcon & use and edit notfound issue

--- src/components/Notfound/Notfound.js
+++ src/components/Notfound/Notfound.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Notfound.css'
 
-const Notfound = () => {
-    return (
+const NotFound = () => {
+  return (
+    <section className=''>
+      <div className=''>
         <div>
-            <h1>This in not found.</h1>
+          <h1>
+            Oops! The page you're looking for isn't here.
+          </h1>
+          <p className=''>
+            You might have the wrong address, or the page may have moved.
+          </p>
+          
         </div>
-    );
-};
+        <div>
+          <div className='error-img'>
+            <img
+              src='https://thumbs.dreamstime.com/b/%C3%B0%C2%BF%C3%B0%C2%B5%C3%B1%E2%80%A1%C3%B0%C2%B0%C3%B1%E2%80%9A%C3%B1%C5%93-134036857.jpg'
+              alt=''
+            />
+          </div>
+          <span className='around-link'>
+          <Link
+            to='/'
+            className='homepage-link'
+          >
+            Back to homepage
+          </Link>
+          </span>
+        </div>
+      </div>
+    </section>
+  )
+}
 
-export default Notfound;
+export default NotFound
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Review.css'
+import { faStar } from '@fortawesome/free-solid-svg-icons'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Review = ({ review }) => {
     const { name, ratting, description } = review
     return (
         <div className='review'>
-            <h4>Name: {name}</h4>
-            <p>Description: {description}</p>
-            <h5>Ratting: {ratting}</h5>
+            <h4>Review By: {name}</h4>
+            <p>" {description} "</p>
+            <h5>Ratting: {ratting}
+            <span className='icon'><FontAwesomeIcon icon={faStar}></FontAwesomeIcon></span>
+            </h5>
         </div>
     );
 };

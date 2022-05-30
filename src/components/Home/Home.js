abb7f5ddceafd7bcf67755c53553977ac5a98f62
jrtanmoy <roy28301@gmail.com>
2022-05-30 18:20:42 +0600

    some update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
             <h1 className='h1-title-style'> Product Reviews !!!</h1>
             <div className="first-section-container">
                 <div className="first-section-container-info">
-                    <h2>Your next Guiter</h2>
+                    <h2>Your Next Guiter</h2>
                     <h2 className='h2-style'>Your Best Guiter</h2>
                     <p>The Epiphone J200 EC Studio pays homage to its Gibson counterpart, with stunning vintage correct stylings. The moustache bridge, decorative tortoiseshell-style pickguard and pearloid crown inlays add a level of class to this instrument and we must say, it sounds as good as it looks, as well. The sound is balanced rather than boomy, giving strummed chords the fullness that made the J-200's reputation. Expect articulate highs and a warm foundation in the bottom end. </p>
                 </div>
